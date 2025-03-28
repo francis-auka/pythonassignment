d162428a94c64f5c91842541051a4b3bcaaafb2c
francis-auka <127690842+francis-auka@users.noreply.github.com>
2025-03-28 21:08:28 +0300

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,22 @@
+my_list = []
+
+# 1. Append elements
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# 2. Insert 15 at the second position
+my_list.insert(1, 15)
+
+# 3. Extend the list
+my_list.extend([50, 60, 70])
+
+# 4. Remove the last element
+my_list.pop()
+
+# 5. Sort the list
+my_list.sort()
+
+# 6. Find and print the index of 30
+print("Index of 30:", my_list.index(30))
